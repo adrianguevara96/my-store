c4a870f68f9de78b2bd713c52bb32f6618fec2df
Adrian Guevara <adrianjga96@gmail.com>
2023-11-14 12:55:19 -0400

    feat: retry request

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
+import { retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
 
   getOne(id: string) {
